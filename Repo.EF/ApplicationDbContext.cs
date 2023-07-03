76173e39cb4b69063e9d77f654bdf895dfe9481f
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-03 02:59:16 +0200

    Add All layers with startup Folders

---
+++ Repo.EF/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.EF
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+
+		}
+
+		public virtual DbSet<Author> Authors { get; set; }
+		public virtual DbSet<Book> Books { get; set; }
+	}
+}
